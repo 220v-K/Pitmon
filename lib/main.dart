41576352b3f2529cdf446b4c863becf2fd3cbbb6
220v-K <jw101902@naver.com>
2021-12-05 21:16:33 +0900

    remove unnece- import

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:pitmon_test/constants.dart';
 import 'package:pitmon_test/pages/homepage.dart';
-import 'package:pitmon_test/pages/mainPage.dart';
 import 'package:pitmon_test/providers/userdata.dart';
 import 'package:provider/provider.dart';
 
